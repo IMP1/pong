499d483ae256334567653d008a495a07a9b5c0b2
IMP1 <IMP1@mail.com>
2016-04-04 00:29:41 +0100

    added window title

--- src/run/Main.java
+++ src/run/Main.java
@@ -2,8 +2,10 @@ package run;
 
 public class Main extends Game {
 
+	
+	
 	public Main() {
-		super(new scn.Arena());
+		super(new scn.Arena(), "Pong 3D");
 	}
 	
 	public static void main(String... args) {
